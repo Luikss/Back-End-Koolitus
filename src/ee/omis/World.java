0583787a8c71d564c8458c9a5084aa24c3d60770
Andre Luige <andreluige@hot.ee>
2021-04-20 20:47:17 +0300

    Player movement and map

---
+++ src/ee/omis/World.java
@@ -0,0 +1,56 @@
+package ee.omis;
+
+import java.util.List;
+
+public class World {
+    private static int height;
+    private static int width;
+    private List<Character> characters;
+
+    public World(int height, int width, List<Character> characters) {
+        World.height = height;
+        World.width = width;
+        this.characters = characters;
+    }
+
+    public static int getHeight() {
+        return height;
+    }
+
+    public static int getWidth() {
+        return width;
+    }
+
+    public List<Character> getCharacters() {
+        return characters;
+    }
+
+    public void setCharacters(List<Character> characters) {
+        this.characters = characters;
+    }
+
+    public void render() {
+        char symbol;
+
+        for (int y = 0; y <= height; y++) {
+            for (int x = 0; x <= width; x++) {
+                if(y == 0 || y == height) {
+                    symbol = '-';
+                } else if (x == 0 || x == width) {
+                    symbol = '|';
+                } else {
+                    symbol = ' ';
+                }
+
+                for (Character c : characters) {
+                    if (c.getxCoord() == x && c.getyCoord() == y) {
+                        symbol = c.getSymbol();
+                    }
+                }
+
+                System.out.print(symbol);
+            }
+            System.out.println();
+        }
+    }
+}
