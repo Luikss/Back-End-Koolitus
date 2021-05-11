22725bf7e6d979f76b50661b69a669a2eeab94bb
Andre Luige <andreluige@hot.ee>
2021-05-11 20:43:57 +0300

    cart services, admin view, admin functions

---
+++ Angular1/webshop-frontend/src/app/admin/item-add/item-add.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-item-add',
+  templateUrl: './item-add.component.html',
+  styleUrls: ['./item-add.component.css']
+})
+export class ItemAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    console.log(form.value);
+    console.log(form.value.title);
+    console.log(form.value.price);
+
+    if (form.valid) {
+    
+    }
+  }
+}
--- Angular1/webshop-frontend/src/app/cart/cart.component.ts
+++ Angular1/webshop-frontend/src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from './cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,11 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  cartItems: {imgSrc: string, title: string, price: number, category: string}[] = [];
+  sumOfCart = 0;
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
     console.log("CART componendis");
+    this.cartItems = this.cartService.itemsInCart;
+    this.calculateSumOfCart();
   }
 
+  onRemoveFromCart(index: number) {
+    this.cartService.itemsInCart.splice(index, 1);
+    this.calculateSumOfCart();
+  }
+
+  onEmptyCart() {
+    this.cartService.itemsInCart = [];
+    this.cartItems = this.cartService.itemsInCart;
+    this.calculateSumOfCart();
+  }
+
+  calculateSumOfCart() {
+    this.sumOfCart = 0;
+    this.cartItems.forEach(cartItem => {
+      this.sumOfCart += cartItem.price;
+    });
+  }
 }
