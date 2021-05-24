37bea660d4f4695cd6d0a5ce7ca65bcf736993c1
Andre Luige <andreluige@hot.ee>
2021-05-24 17:55:03 +0300

    Angular exercise 9

---
+++ Angular1/harjutus9/src/app/vastus/vastus.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { CalculationService } from '../calculation.service';
+
+@Component({
+  selector: 'app-vastus',
+  templateUrl: './vastus.component.html',
+  styleUrls: ['./vastus.component.css']
+})
+export class VastusComponent implements OnInit {
+  answer: number = 0;
+
+  constructor(private calculation: CalculationService) { }
+
+  ngOnInit(): void {
+    this.calculation.answerChanged.subscribe(newAnswer => {
+      this.answer = newAnswer;
+    })
+  }
+}
