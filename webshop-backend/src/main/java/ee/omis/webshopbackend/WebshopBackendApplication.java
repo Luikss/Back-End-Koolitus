08857ebb8833f699346c0f6d54a16db6ef8e359c
Andre Luige <andreluige@hot.ee>
2021-06-01 20:35:18 +0300

    Webshop backend start

---
+++ webshop-backend/src/main/java/ee/omis/webshopbackend/WebshopBackendApplication.java
@@ -0,0 +1,14 @@
+package ee.omis.webshopbackend;
+
+import ee.omis.webshopbackend.model.Item;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopBackendApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopBackendApplication.class, args);
+    }
+}
